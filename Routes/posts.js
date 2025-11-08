a37662d29273c0d10ac47f41e387b36d7a5bb2c7
Bohemius1272 <jerryknyazhevsky@gmail.com>
2025-11-08 15:44:13 -0500

    fifth commit

--- Routes/posts.js
+++ Routes/posts.js
@@ -4,3 +4,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.send('all posts will be displayed here.');
 });
+
+router.get('/new', (req, res) => {
+    res.send('Form to create a new post will be shown here.');
+});
+
+MediaSourceHandle.exports = router;
