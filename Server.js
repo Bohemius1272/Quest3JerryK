f1ef682d0b530f9f57a1611ee5c789d7bcb993be
Bohemius1272 <jerryknyazhevsky@gmail.com>
2025-11-08 15:52:05 -0500

    67 COMMITS!!!

--- Server.js
+++ Server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const userRoutes = require('./routes/users');
-const postRoutes = require('./routes/posts');
+const userRoutes = require('/routes/users');
+const postRoutes = require('/routes/posts');
 
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the main page!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
